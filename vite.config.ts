b7e086737e8f068b53a23b4320dbbddb345e80d0
Kkk <2568143457@qq.com>
2023-02-02 10:50:56 +0800

    chore: 添加github pages的项目资源前缀

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
+import { fileURLToPath } from 'url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({ 
-      registerType: 'autoUpdate',
-      srcDir: 'src',
-      filename: 'sw.ts',
-      strategies: 'injectManifest',
-      manifest: {
-        name: 'Notice Book',
-        short_name: 'Notice Book',
-        theme_color: '#ffffff'
-      },
+export default defineConfig(({ mode }) => {
+  return {
+    base: loadEnv(mode, __dirname).VITE_APP_PATHNAME,
+    plugins: [
+      vue(),
+      VitePWA({ 
+        registerType: 'autoUpdate',
+        srcDir: 'src',
+        filename: 'sw.ts',
+        strategies: 'injectManifest',
+        manifest: {
+          name: 'Notice Book',
+          short_name: 'Notice Book',
+          theme_color: '#ffffff'
+        },
       // devOptions: {
       //   enabled: true,
       //   type: 'module',
       //   navigateFallback: 'index.html'
       // }
-    })
-  ],
+      })
+    ],
+  }
 })
