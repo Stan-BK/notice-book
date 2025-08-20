a5ac1b3792de4adf83a6084916ddaad2dd51f789
libondev <bon.li@outlook.com>
2025-08-20 12:32:10 +0800

    chore: improve UI style

--- src/components/NtList.vue
+++ src/components/NtList.vue
@@ -3,10 +3,6 @@ import { NoticeType } from '../libs'
 
 const props = defineProps<{ notices: NoticeType[] }>()
 
-function handleItem(notice: NoticeType) {
-  notice.isChosen = !notice.isChosen
-}
-
 function validNum(type: 'hour' | 'minute', notice: NoticeType, e: Event) {
   const num = type === 'hour' ? 23 :  59
   const tar = e.target as HTMLInputElement
@@ -25,6 +21,7 @@ function blur(e: Event) {
   tar.value = tar.value.padStart(2, '0')
 }
 </script>
+
 <template>
   <TransitionGroup
     name="list"
@@ -32,26 +29,23 @@ function blur(e: Event) {
     class="notice-list w-full"
   >
     <li
-      v-for="(notice) in props.notices"
-      :key="notice"
-      class="notice-list-item flex items-center"
+      v-for="(notice, i) in props.notices"
+      :key="notice.noticeName + i"
+      class="notice-list-item flex items-center gap-3"
     >
-      <div
-        class="drag-btn-wrap"
-        :class="notice.isChosen ? 'chosen' : ''"
-        @click="handleItem(notice)"
-      >
-        <span class="drag-btn" />
-      </div>
-      <input
+      <PCheckbox v-model="notice.isChosen" />
+
+      <PInput
         v-model="notice.noticeName"
-        class="notice-input noticeName"
-      >
+        class="noticeName"
+      />
+
       <textarea
         v-model="notice.description"
         class="notice-input description"
         style="resize: none;"
       />
+
       <div class="notice-input timestamp">
         <input
           v-model="notice.hour"
@@ -72,9 +66,11 @@ function blur(e: Event) {
     </li>
   </TransitionGroup>
 </template>
+
 <style scoped lang="less">
 ul {
   padding: 0;
+  margin: 0;
 }
 .notice-list {
   flex: 1;
@@ -90,55 +86,12 @@ ul {
     border-radius: 8px;
     white-space: nowrap;
 
-    .drag-btn-wrap {
-      display: inline-flex;
-      width: 6%;
-      height: 50px;
-      justify-content: center;
-      align-items: center;
-      cursor: pointer;
-
-      .drag-btn {
-        position: relative;
-        display: inline-block;
-        width: 15px;
-        height: 15px;
-        border: 1px solid var(--color-gray-600);
-        transition: background-position .2s;
-        overflow: hidden;
-        transition: .2s;
-
-        &::after {
-          display: block;
-          content: "";
-          position: absolute;
-          width: 200%;
-          height: 200%;
-          background-color: var(--color-gray-600);
-          left: -200%;
-          top: 200%;
-          transition: .2s;
-          transform: rotate(-135deg);
-        }
-
-      }
-
-      &.chosen .drag-btn::after, &:hover .drag-btn::after {
-        left: -50%;
-        top: -50%;
-      }
-
-      &.chosen .drag-btn::after {
-        background-color: var(--hover-color);
-      }
-
-      &.chosen .drag-btn, &:hover .drag-btn {
-        transform: rotate(45deg);
-      }
+    .noticeName {
+      width: 25%;
     }
 
     .notice-input {
-      height: 50px;
+      height: 36px;
       line-height: 50px;
       padding: 4px;
       background-color: transparent;
@@ -152,10 +105,6 @@ ul {
 
       &:focus {
         border-color: var(--color-gray-600);
-      }
-
-      &.noticeName {
-        width: 20%;
       }
 
       &.timestamp {
@@ -175,8 +124,7 @@ ul {
       }
 
       &.description {
-        width: 50%;
-        margin: auto 2%;
+        flex: 1;
         line-height: 32px;
         &:focus {
           height: 150px;
