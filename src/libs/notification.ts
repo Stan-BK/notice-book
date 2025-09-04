8909bd545f09d813f4a27c044d8020ff38e6cf62
Stan-BK <2568143457@qq.com>
2025-09-04 11:26:57 +0800

    chore: 修改通知权限获取的结果提示

--- src/libs/notification.ts
+++ src/libs/notification.ts
@@ -1,16 +1,19 @@
+import { useMessage } from 'pxd'
+
 export function initNotification() {
-  return new Promise(resolve => {
+  return new Promise(() => {
     Notification.requestPermission().then(status => {
       switch(status) {
 
       case 'default': {
-        alert('To guarantee the whole powers of Notice-Book, please approve the authority of \'Notifaction\'')
-        console.log('The permission request was dismissed.')
-        return
+        useMessage('Make sure the authority of \'Notifaction\' has been approved', {
+          type: 'warning',
+        })
       } break
       case 'denied': {
-        alert('To guarantee the whole powers of Notice-Book, please approve the authority of \'Notifaction\'')
-        console.log('Permission wasn\'t granted. Allow a retry.')
+        useMessage('To guarantee the whole powers of Notice-Book, please approve the authority of \'Notifaction\'', {
+          type: 'error',
+        })
         return
       } break
       
